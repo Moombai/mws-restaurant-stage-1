869eb6add3d3bf44310b7b6c8bdbbbab161a7c87
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-05-21 11:04:18 +0100

    Add grid layout to restaurants section

--- js/main.js
+++ js/main.js
@@ -138,27 +138,36 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const divRow = document.createElement('div');
+  divRow.className = 'row';
+
+  const divColumn = document.createElement('div');
+  divColumn.className = 'col six';
+
+  li.append(divRow);
+  divRow.append(divColumn);
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  divColumn.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  divColumn.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  divColumn.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  divColumn.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  divColumn.append(more);
 
   return li
 }
